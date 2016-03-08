c0b19ff3892812d2ff031fb23bd2f29cd6c0e29d
JonAbrego <abrego.fc@gmail.com>
2016-03-07 21:27:46 -0600

    Al parecer ya quedo

--- Haskell/Practica01Jonathan.hs
+++ Haskell/Practica01Jonathan.hs
@@ -32,7 +32,7 @@ length1 (x:xs) = 1+(length1 xs)
 minList:: [Nat]-> Nat
 minList [Cero] = Cero
 minList [Suc x] = Suc x
-minList (x:xs) = if (menor x (minList xs))==True 
+minList (x:xs) = if (menorIgual x (minList xs))==True 
 					then x else minList(xs)
 
 sumList :: [Nat]-> Nat
@@ -42,8 +42,18 @@ sumList [Suc x] = Suc x
 sumList (x:xs) = suma x (sumList(xs))
 
 --Ordenar
+ordena :: [Nat] -> [Nat]
+ordena [] = []
+ordena (x:xs) = insert x (ordena xs)
 
 --Funciones Auxiliares
+insert :: Nat->[Nat]->[Nat]
+insert Cero []= [Cero]
+insert (Suc e) [] = [Suc e]
+insert e (x:xs)
+				| menorIgual e x = e:x:xs				
+				| otherwise = x:insert e xs
+
 convierte :: Nat->Integer
 convierte Cero = 0
 convierte (Suc x) = 1 +convierte x
@@ -52,24 +62,12 @@ revierte :: Integer -> Nat
 revierte 0 =Cero
 revierte (n + 1) = Suc (revierte n)
 
-menor:: Nat -> Nat -> Bool
-menor Cero (Suc n)= if (convierte Cero) < (convierte (Suc n)) 
+menorIgual:: Nat -> Nat -> Bool
+menorIgual Cero Cero= if (convierte Cero) == (convierte Cero) 
 						then True else False
-menor (Suc n) Cero = if (convierte (Suc n)) <(convierte Cero)
+menorIgual Cero (Suc n)= if (convierte Cero) < (convierte (Suc n)) 
 						then True else False
-menor (Suc n) (Suc y) = if (convierte (Suc n)) < (convierte (Suc y))
+menorIgual (Suc n) Cero = if (convierte (Suc n)) < (convierte Cero)
+						then True else False
+menorIgual (Suc n) (Suc y) = if (convierte (Suc n)) <= (convierte (Suc y))
 						then True else False						
-
-mayor:: Nat -> Nat -> Bool
-mayor Cero (Suc n) = if (convierte Cero) > (convierte (Suc n))
-						then True else False
-mayor (Suc n) Cero = if (convierte (Suc n)) > (convierte Cero) 
-						then True else False
-mayor (Suc n) (Suc y) = if (convierte (Suc n)) > (convierte (Suc y)) 
-						then True else False
-
-igual :: Nat->Nat->Bool												
-igual Cero Cero = if (convierte Cero)==(convierte Cero) 
-					then True else False
-igual (Suc x)(Suc y)= if (convierte (Suc x)) == (convierte (Suc y))
-						then True else False
