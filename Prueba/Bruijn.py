8f57e1276670274394dfac9f85e3617b34fa1f59
JonAbrego <abrego.fc@gmail.com>
2016-04-24 01:19:40 -0500

    Proyecto de Algoritmos
    Calculo de la secuencia de De Brujin, además de la impresion correspondiente
    de la grafica asociada según los parametros m,n

--- Prueba/Bruijn.py
+++ Prueba/Bruijn.py
@@ -1,72 +1,73 @@
 #!/usr/bin/env python
-# try:
-#     import matplotlib.pyplot as plt
-# except:
-#     raise
-# import networkx as nx
-# try:    
-#     m=nx.drawing.write_dot.__module__
-# except:      
-#     raise
-# import sys
-
-
-# def debruijn(m, n):
-#     v = [0 for _ in range(n)]
-#     l = 1
-#     r = []
-#     while True:
-#         if n % l == 0:
-#           r.extend(v[0:l])
-#         for i in range(l, n):
-#             v[i] = v[i-l]
-#         l = n
-#         while l > 0 and v[l-1] >= m-1:
-#            l-=1
-#         if l == 0:
-#             break
-#         v[l-1] += 1
-#     return r
-
-# def grafica(m,n):    
-    # num_vertices=(m**(n-1))
-    # num_alfabeto=(n-1)
-    # alfabeto = []
-    # for i in range(0,num_alfabeto):
-    #     alfabeto.append(i)        
-    # G=nx.MultiDiGraph(loops=True)    
-    # for i in range(0,m**(n-1)):
-    #     G.add_node(i)           
-    # pos=nx.spring_layout(G) # positions for all nodes
-    # nx.draw_networkx_nodes(G,pos,node_size=700)
-    # nx.draw_networkx_edges(G,pos)    
-    # nx.draw_networkx_labels(G,pos,font_size=12)    
-    # plt.axis('off')        
-    # plt.savefig("weighted_graph.png") # save as png
-    # plt.show() # display    
-
-# def main():
-#     try: #verifica que le pasen los parametros requeridos
-#         m = sys.argv[1]
-#         n = sys.argv[2]
-#     except:
-#         print "Brujin necesita dos enteros un m y n positivos "        
-#         print "python "+ sys.argv[0] + " m:entero n:entero"
-#         print "Por ejmeplo:\npython "+ sys.argv[0] + " 2 3"
-#         quit()        
-#     print debruijn(int(m),int(n))            
-#     grafica(int(m),int(n))            
-
-# if __name__ == '__main__':
-#     main()
-#!/usr/bin/env python
 from graphviz import Digraph
-dot = Digraph(comment='The Round Table')
-dot.node('A', 'King Arthur')
-dot.node('B', 'Sir Bedevere the Wise')
-dot.node('L', 'Sir Lancelot the Brave')
+import sys
 
-dot.edges(['AA', 'AL'])
-dot.edge('B', 'L', constraint='false')
-dot.render('test-output/round-table.gv', view=True)
-'test-output/round-table.gv.gpg'
+def de_bruijn(m , n):        
+    #determinamos nuestro alfabeto 
+    alfabeto = list(map(str, range(m))) 
+    a = [0] * m * n
+    secuencia = []
+    def db(t, p):
+        if t > n:
+            if n % p == 0:
+                secuencia.extend(a[1:p + 1])
+        else:
+            a[t] = a[t - p]
+            db(t + 1, p)
+            for j in range(a[t - p] + 1, m):                
+                a[t] = j
+                db(t + 1, t)
+    db(1, 1)
+    for j in range(0, n - 1):
+        a[j] = 0
+    #
+    secuencia.extend(a[0:n - 1])
+    #para la secuencia dada la unimos a una cadena vacia para
+    #imprimirlo de una manera mas adecuada
+    return "".join(alfabeto[i] for i in secuencia)
+
+def lista_vertices(c, m , n):
+    v=[]
+    for i in range(0,m**n):
+        if c[i:i+(n-1)] not in v and len(c[i:i+(n-1)])==(n-1):
+            v.insert(i,c[i:i+(n-1)])
+    return v            
+
+def lista_aristas(c, m, n):
+    e=[]    
+    for i in range(0,m**n):
+        if c[i:i+n] not in e and len(c[i:i+n])==n:
+            e.insert(i,c[i:i+n])               
+    return e
+
+def grafica(c,m,n):
+    dot = Digraph(format='pdf')
+    ver=lista_vertices(c, m , n)
+    for i in range(0,len(ver)):
+        dot.node(ver[i],ver[i])          
+    for i in range(0,len(ver)):
+        elemento = ver[i]        
+        for j in range(0,len(ver)):
+            elemento2 = ver[j]
+            #Desde aqui
+            arista=elemento[0:1]+elemento2[0:(n-1)]            
+            if arista[0:(n-1)]==elemento and arista[1:n]==elemento2:
+                dot.edge(elemento,elemento2,label=arista)        
+    dot.render(view=True)        
+
+def main():
+    #verifica que le pasen los parametros requeridos
+    try: 
+        m = sys.argv[1]
+        n = sys.argv[2]
+    except:
+        print "Brujin necesita dos enteros un m y n positivos "        
+        print "python "+ sys.argv[0] + " m:entero n:entero"
+        print "Por ejmeplo:\npython "+ sys.argv[0] + " 2 3"
+        quit()        
+    #llamada de metodo para calcular la secuencia de G_{m,n}    
+    print de_bruijn(int(m),int(n))    
+    grafica(de_bruijn(int(m),int(n)),int(m),int(n))        
+
+if __name__ == '__main__':
+    main()
